91c4212d320304dd18b87528cd8cb644ae48dafb
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2020-06-23 19:50:51 -0500

    [TEST] Remove superfluous jest settings

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,5 @@ const { defaults } = require('jest-config');
 module.exports = {
   preset: "ts-jest",
   clearMocks: true,
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-  testRegex: "./spec/.*\\.(spec).(ts|tsx)$",
-  testPathIgnorePatterns: ["/node_modules/"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  }
+  testRegex: "./spec/.*\\.(spec).(ts|tsx)$"
 };
