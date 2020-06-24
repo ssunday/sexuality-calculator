f28ae29706a4f77ca5312acaafc1f189b3520fd3
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2020-06-23 19:40:59 -0500

    [SITE] More copy/doc updates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sexuality Calculator
 
-This is still WIP, language very rough, needs to be better, I know. If you have suggestions, please leave an issue or pull request.
+This is still vaguely WIP. The questions could be phrased better for sure. If you have suggestions, please leave an issue or pull request.
 
 Built with [React Static](https://github.com/react-static/react-static), [styled-components](https://styled-components.com), and TypeScript.
 
--- src/calculators/sexuality/index.tsx
+++ src/calculators/sexuality/index.tsx
@@ -69,14 +69,12 @@ const SexualityCalculatorComponent = (): JSX.Element => {
     <>
       <p>
         Answer each question to the best of your ability, choosing the most
-        applicable response. The results will come in the form of any possible
-        sexuality that matches your responses.
+        applicable response. The results will come in the form of possible
+        sexualities, hopefully helping you figure out the right term. Whatever the results, only you can know and decide for certain. This is just to try to help.
       </p>
       <p>
-        Whatever the results, only you can know and decide for certain. This is
-        just to try to help! If you see an isssue, make a pull
-        request or issue on{" "}
-        <a href="https://github.com/ssunday/sexuality-calculator">Github</a>.
+        If you see an issue or have a suggestion, please make a pull request or issue
+        on <a href="https://github.com/ssunday/sexuality-calculator">Github</a>.
       </p>
       {renderQuestions(initialQuestions())}
       {renderQuestions(applicableQuestions)}
